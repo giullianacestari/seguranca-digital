29981bbef54f84140e20687b873bc9fa081a784d
Giulliana Salvanhini Cestari <dev.giullianacestari@gmail.com>
2026-03-23 16:59:26 -0300

    ler em voz alta

--- script.js
+++ script.js
@@ -36,4 +36,47 @@ function diminuiFonte() {
   tamanhoFonteAtual = tamanhoFonteAtual - valor;
   document.documentElement.style.fontSize = `${tamanhoFonteAtual}px`;
 }
-=======
+
+// LEITURA EM VOZ ALTA
+
+// variável que guarda se o sistema está lendo
+let lendo = false;
+const botaoLer = document.getElementById("btn-leitura");
+
+// quando clicar no botão
+botaoLer.addEventListener("click", iniciarLeitura);
+
+function iniciarLeitura() {
+  // se já estiver lendo, pausa ou continua a leitura
+  if (lendo == true) {
+    if (speechSynthesis.paused == true) {
+      speechSynthesis.resume();
+    } else {
+      speechSynthesis.pause();
+    }
+    return;
+  }
+
+  // pegar o texto da página
+  let conteudo = document.querySelector("main");
+  let texto = conteudo.innerText;
+
+  // criar objeto de fala
+  let fala = new SpeechSynthesisUtterance(texto);
+  fala.lang = "pt-BR";
+
+  // quando terminar de ler, chamar função de finalizar leitura
+  fala.onend = finalizarLeitura;
+  lendo = true;
+
+  // cancelar falas antigas
+  speechSynthesis.cancel();
+
+  // iniciar leitura
+  speechSynthesis.speak(fala);
+}
+
+// função chamada quando a leitura termina
+function finalizarLeitura() {
+  lendo = false;
+}
