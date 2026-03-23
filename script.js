12005a88d3445356e2c751ecab7a5eadd38fd469
Giulliana Salvanhini Cestari <dev.giullianacestari@gmail.com>
2026-03-23 11:42:51 -0300

    tamanho de fontes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// MODAL DE AJUDA
+
 let botaoAjuda = document.querySelector(".btn-help");
 let botaoFechar = document.querySelector(".btn-close");
 let janelaDeAjuda = document.querySelector(".modal-fundo");
@@ -11,4 +13,25 @@ function abrirModal() {
 
 function fecharModal() {
   janelaDeAjuda.style.display = "none";
+}
+
+// AUMENTAR/DIMINUIR FONTE
+
+let tamanhoFonteAtual = 16;
+const valorAdicionado = 2; // Quantos pixels aumenta/diminui por clique
+
+const btnAumentar = document.getElementById("btn-aumentar-fonte");
+const btnDiminuir = document.getElementById("btn-diminuir-fonte");
+
+btnAumentar.addEventListener("click", aumentaFonte);
+btnDiminuir.addEventListener("click", diminuiFonte);
+
+function aumentaFonte() {
+  tamanhoFonteAtual = tamanhoFonteAtual + valorAdicionado;
+  document.documentElement.style.fontSize = `${tamanhoFonteAtual}px`;
+};
+
+function diminuiFonte() {
+  tamanhoFonteAtual = tamanhoFonteAtual - valorAdicionado;
+  document.documentElement.style.fontSize = `${tamanhoFonteAtual}px`;
 }
